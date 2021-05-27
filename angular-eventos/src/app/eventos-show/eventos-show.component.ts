034c3ae75c72b420f001f8cf43ce0bea2d8448d9
Inma <inm.macu@hotmail.com>
2021-05-27 16:33:05 +0200

    Generamos componente e interfaz

---
+++ angular-eventos/src/app/eventos-show/eventos-show.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { IEvento } from '../interfaces/ievento';
+
+@Component({
+  selector: 'app-eventos-show',
+  templateUrl: './eventos-show.component.html',
+  styleUrls: ['./eventos-show.component.css']
+})
+
+export class EventosShowComponent implements OnInit {
+  //title = "Mi lista de eventos";
+
+  misEventos: IEvento[] = [
+    {
+      title: "Salimos a correr",
+      image: "assets/correr.jpg",
+      date: "2021-01-23",
+      description: "Nos lo pasaremos genial",
+      price: 5
+    }, {
+      title: "Sintamos la música",
+      image: "assets/musica.jpeg",
+      date: "2020-01-24",
+      description: "Déjate llevar",
+      price: 8.5
+    },{
+      title: "Papiroflexia",
+      image: "assets/papiroflexia.jpg",
+      date: "2021-01-30",
+      description: "Encuentra lo que estás buscando",
+      price: 1
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
