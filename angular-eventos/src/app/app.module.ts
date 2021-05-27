e5d71f0812409699a7955729419d5a9a73088ba3
Inma <inm.macu@hotmail.com>
2021-05-27 20:46:50 +0200

    Hasta aquí práctica 1

--- angular-eventos/src/app/app.module.ts
+++ angular-eventos/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventosShowComponent } from './eventos-show/eventos-show.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { EventosShowComponent } from './eventos-show/eventos-show.component';
     EventosShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-eventos/src/app/eventos-show/eventos-show.component.ts
+++ angular-eventos/src/app/eventos-show/eventos-show.component.ts
@@ -29,7 +29,13 @@ export class EventosShowComponent implements OnInit {
       date: "2021-01-30",
       description: "Encuentra lo que estás buscando",
       price: 1
-    },
+    },{
+      title: "Examen Angular",
+      image: "assets/pajaros.jpg",
+      date: "2021-06-23",
+      description: "Sacaremos mínimo un 5",
+      price: 100
+    }
   ];
 
   constructor() { }
